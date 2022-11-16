aeabfb4f8004480244f441c6542963c9f512369e
Queenesther <umohqueenesther@gmail.com>
2022-11-16 14:28:45 +0100

    adding media queries

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -6,20 +6,22 @@ function Modal({ setShowBox }){
     <div className="modal">
       <div className="modal__outline">
         <div className="modal__header">
-          <h2>Connect Wallet</h2>
+          <h2 >Connect Wallet</h2>
           <button
             onClick={ () => setShowBox(false)}
-          > 
-            x
+         className="modalbtn" > 
+           <img src="img/x.svg" alt="btnimg"/> 
           </button>
         </div> 
         <div className="modal__content">
-          <h5>Choose your preferred wallet: </h5>
+          <h4>Choose your preferred wallet: </h4>
           <form action="#" className="modal__form">
+          <div className="imagediv">
           <input type="text" placeholder="Metamask 	&#62;" className="modal__metamask" id=" #myinput"/>
           <img src="img/image 66.svg" alt="metamask" className="metamaskimg"/>
           <input type="text" placeholder="Connect wallet" className="modal__wallet"/>
-          <img src="img/image 69.svg" alt="wallet" className="wallet"/>
+          <img src="img/image 69.svg" alt="wallet" className="wallet"/> 
+          </div>
           </form>
         </div> 
       </div> 
